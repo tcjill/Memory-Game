3fbef07167eb206f08215653d7b3dc328e1ca01a
jill <jgundaker@gmail.com>
2018-05-28 10:01:33 -0700

    iamplayingwiththetimer

--- js/app.js
+++ js/app.js
@@ -109,26 +109,8 @@ function notMatching(){
 
 
 //timer//
-var timer = new Timer();
-}var h2 = document.getElementsByTagName('h2')[0],
-start = document.getElementById('start'),
-stop = document.getElementById('stop'),
-clear = document.getElementById('clear'),
-seconds = 0, minutes = 0, hours = 0,
-t;
+//var timer = new Timer();
 
-function add() {
-seconds++;
-if (seconds >= 60) {
-    seconds = 0;
-    minutes++;
-    if (minutes >= 60) {
-        minutes = 0;
-        hours++;
-    }
-}
-
-h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 
 timer();
 }
@@ -138,18 +120,13 @@ t = setTimeout(add, 1000);
 
 
 
-/* Start button */
-start.onclick = timer;
 
-/* Stop button */
-stop.onclick = function() {
-clearTimeout(t);
-}
+//restart button???
 
-/* Clear button */
-clear.onclick = function() {
-h2.textContent = "00:00:00";
-seconds = 0; minutes = 0; hours = 0;
+function restartGame() {
+	restart.on('click', function() {
+		startGame();
+	})
 }
 
 // Move Counter function
