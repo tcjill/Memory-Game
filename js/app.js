c9986201d7446c7b906c3aba8e84a9d76f022b96
jill <jgundaker@gmail.com>
2018-05-29 16:13:00 -0700

    deleted notes

--- js/app.js
+++ js/app.js
@@ -2,17 +2,21 @@ const time = document.getElementById('time');
 // t is a global variable therefore its val can be accessed anywhere
 let t = 0;
 
+
 function Timer () {
   var timer = setInterval(function() {
         console.log(t);
         t++;
         if(t >= 0) {
           time.innerHTML = t;
-          //here you could put other conditionals to make mins or whatever
+        
+          //here could put other conditionals to make mins or whatever
            /*clearInterval(timer);*/
+
         }
     }, 1000);
 }
+
 var timer = new Timer();
 console.log(t);
 
@@ -125,49 +129,13 @@ function notMatching(){
    // const time = document.getElementById('time');
 
 
-// //timer//
-// var timer = new Timer();
-// }var h2 = document.getElementsByTagName('h2')[0],
-// start = document.getElementById('start'),
-// stop = document.getElementById('stop'),
-// clear = document.getElementById('clear'),
-// seconds = 0, minutes = 0, hours = 0,
-// t;
 
-// function add() {
-// seconds++;
-// if (seconds >= 60) {
-//     seconds = 0;
-//     minutes++;
-//     if (minutes >= 60) {
-//         minutes = 0;
-//         hours++;
-//     }
+
+
 }
 
-// h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-
-// timer();
-// }
-// function timer() {
-// t = setTimeout(add, 1000);
-// }
 
 
-
-// /* Start button */
-// start.onclick = timer;
-
-// /* Stop button */
-// stop.onclick = function() {
-// clearTimeout(t);
-// }
-
-// /* Clear button */
-// clear.onclick = function() {
-// h2.textContent = "00:00:00";
-// seconds = 0; minutes = 0; hours = 0;
-// added comment and want to commit to timer
 
 // Move Counter function
 function moveCounter(){
