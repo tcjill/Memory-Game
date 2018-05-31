d45b7a0b3d97639eff2410aaf6e8225c2952335d
jill <jgundaker@gmail.com>
2018-05-31 08:37:23 -0700

    prettified

--- js/app.js
+++ js/app.js
@@ -4,17 +4,18 @@ let t = 0;
 
 const restart = document.querySelector('.restart');
 
-function Timer () {
-  var timer = setInterval(function() {
-        console.log(t);
-        t++;
-        if(t >= 0) {
-          time.innerHTML = t;
-        
-          //here could put other conditionals to make mins or whatever
-           /*clearInterval(timer);*/
+function Timer() { 
+    var timer = setInterval(function() {    
+        console.log(t);    
+        t++;    
+        if (t >= 0) {     
+            time.innerHTML = t;
 
-        }
+            //here could put other conditionals to make mins or whatever
+                  /*clearInterval(timer);*/
+
+               
+        }
     }, 1000);
 }
 
@@ -23,30 +24,31 @@ console.log(t);
 
 
 // List of all cards
-var allCards = ['fa-diamond','fa-diamond',
-              'fa-paper-plane-o','fa-paper-plane-o',
-              'fa-anchor','fa-anchor',
-              'fa-bolt','fa-bolt',
-              'fa-cube','fa-cube',
-              'fa-leaf','fa-leaf',
-              'fa-bicycle','fa-bicycle',
-              'fa-bomb','fa-bomb'
-              ];
+var allCards = ['fa-diamond', 'fa-diamond',
+    'fa-paper-plane-o', 'fa-paper-plane-o',
+    'fa-anchor', 'fa-anchor',
+    'fa-bolt', 'fa-bolt',
+    'fa-cube', 'fa-cube',
+    'fa-leaf', 'fa-leaf',
+    'fa-bicycle', 'fa-bicycle',
+    'fa-bomb', 'fa-bomb'
+];
 
 // Moves variables
 var moveCounter = document.querySelector('.moves');
 var moves = 0;
-  
+
 // Keep track of cards flipped
 var openCards = [];
 
 //Load new game and then shuffle
 newGame();
 
- /*   - shuffle the list of cards using the provided "shuffle" method below*/
+/*   - shuffle the list of cards using the provided "shuffle" method below*/
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -63,98 +65,98 @@ function shuffle(array) {
  *   - loop through each card and create its HTML Maybe re do again?? Look it up later
  */
 function makeCard(card) {
-  return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
- 
-function newGame(){
-  var deck = document.querySelector('.deck');
-  var cardGrid = shuffle(allCards).map(function(card) {
-    return makeCard(card);
-  });  
-  deck.innerHTML = cardGrid.join('');
+
+function newGame() {
+    var deck = document.querySelector('.deck');
+    var cardGrid = shuffle(allCards).map(function(card) {
+        return makeCard(card);
+    });
+    deck.innerHTML = cardGrid.join('');
 }
 
 /*
  * set up the event listener for a card. If a card is clicked:
  */
 var cardList = document.querySelectorAll('.card');
-for(let card of cardList) {
-  card.addEventListener('click', function(flipCard){
-    //Disable clicking on the same card
-    if (!card.classList.contains('open') &&     !card.classList.contains('show') && !card.classList.contains('match')){
-      currentT = t;
-  //Add the card to a *list* of "open" cards
-    openCards.push(card);
+for (let card of cardList) {
+    card.addEventListener('click', function(flipCard) {
+        //Disable clicking on the same card
+        if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+            currentT = t;
+            //Add the card to a *list* of "open" cards
+            openCards.push(card);
 
-  //Prevent from showing more than two cards
-      if (openCards.length > 2){
-        //hide
-      } else {
-        //Show cards
-        card.classList.add('open','show');
-        //Check if cards match
-        if (openCards[0].dataset.card == openCards[1].dataset.card){
-            cardMatch();
-        } else {
-            notMatching();
+            //Prevent from showing more than two cards
+            if (openCards.length > 2) {
+                //hide
+            } else {
+                //Show cards
+                card.classList.add('open', 'show');
+                //Check if cards match
+                if (openCards[0].dataset.card == openCards[1].dataset.card) {
+                    cardMatch();
+                } else {
+                    notMatching();
+                }
+            }
         }
-        }
-      }
     });
-  
-// cardMatch function
-function cardMatch(){
-  openCards[0].classList.add('match');
-  openCards[0].classList.add('open');
-  openCards[0].classList.add('show');
 
-  openCards[1].classList.add('match');
-  openCards[1].classList.add('open');
-  openCards[1].classList.add('show');
-  
-  //empty  array
-  openCards = [];
-}
-//reset and restart
-restart.addEventListener ('click', reset);
+    // cardMatch function
+    function cardMatch() {
+        openCards[0].classList.add('match');
+        openCards[0].classList.add('open');
+        openCards[0].classList.add('show');
 
-function reset() {
-  location.reload();
-}
-// Not matching function
-function notMatching(){
+        openCards[1].classList.add('match');
+        openCards[1].classList.add('open');
+        openCards[1].classList.add('show');
 
-//Flip over after a bit more than 1/2second
-  setTimeout(function(){
-    for (let card of openCards){
-      card.classList.remove('open','show');
+        //empty  array
+        openCards = [];
     }
-    moveCounter();
-    openCards = []; //Empty openCards array
-   }, 700);
-   // const time = document.getElementById('time');
+    //reset and restart
+    restart.addEventListener('click', reset);
+
+    function reset() {
+        location.reload();
+    }
+    // Not matching function
+    function notMatching() {
+
+        //Flip over after a bit more than 1/2second
+        setTimeout(function() {
+            for (let card of openCards) {
+                card.classList.remove('open', 'show');
+            }
+            moveCounter();
+            openCards = []; //Empty openCards array
+        }, 700);
+        // const time = document.getElementById('time');
 
 
-}
+    }
 
-const stars = document.querySelector('.stars');
-let star = 1;
-// star -= 1;
+    const stars = document.querySelector('.stars');
+    let star = 1;
+    // star -= 1;
 
-for (let i = 1; i <= 5; i++) {
-    const starLi = document.createElement('li');
-    const starI = document.createElement('i');
-    starI.classList.add('fa', 'fa-star');
-    starLi.appendChild(starI);
-    stars.appendChild(starLi);
-}  
-makeStars(star);
+    for (let i = 1; i <= 5; i++) {
+        const starLi = document.createElement('li');
+        const starI = document.createElement('i');
+        starI.classList.add('fa', 'fa-star');
+        starLi.appendChild(starI);
+        stars.appendChild(starLi);
+    }
+    makeStars(star);
 
 
-// Move Counter function
-function moveCounter(){
-  moves++;
-  moveCounter.innerHTML = moves;
-}
+    // Move Counter function
+    function moveCounter() {
+        moves++;
+        moveCounter.innerHTML = moves;
+    }
 
 }
