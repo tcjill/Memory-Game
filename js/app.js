681fd4586a2bd1c4c69233dfce3151e96e230c2c
jill <jgundaker@gmail.com>
2018-06-03 18:55:44 -0700

    cards won't click

--- js/app.js
+++ js/app.js
@@ -14,17 +14,19 @@ function Timer() {
             //here could put other conditionals to make mins or whatever
             /*clearInterval(timer);*/
 
-      //  }
-    //}, 1000);
+       // }
+   // }, 1000);
 //}
 //stop timer function
 //function stopTimer() {
-   // clearInterval(gameTimer);
+    //clearInterval(gameTimer);
 //}
 //var timer = new Timer();
 //console.log(t);
 
+
 // Timer functions
+
 let sec = 0;
 let min = 0;
 let timer;
@@ -54,6 +56,20 @@ function insertTime() {
 	// display time
 	document.querySelector('timerOutput').innerHTML = "0" + min + ":" + sec;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -185,6 +201,42 @@ for (let card of cardList) {
 
 
     }
+
+  // Get the modal
+
+
+  /*congratulations when all cards match, show modal and moves, time and rating....I need to do this somehow....
+function congratulations(){
+    if (matchedCard.length == 16){
+        clearInterval(interval);
+        finalTime = timer.innerHTML;
+    //show congratulations modal
+    modal.classList.add("show");*/
+
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}  
 
     const starRating = document.getElementsByClassName('stars')[0];
 
